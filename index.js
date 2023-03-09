bb9cb136f402be2df41f8383afe8ed2d8428bbbb
Parker D Van Every <pdvanevery@gmail.com>
2023-03-09 15:00:55 -0800

    serach button brings up test pokemon

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-.then((res) => res.json())
-.then((data) => console.log(data))
+let btn = document.querySelector("#btn");
+let div = document.querySelector("#name");
+
+btn.addEventListener('click', getPokemon)
+
+function getPokemon() {
+
+    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+}
