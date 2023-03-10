f2fade03e303a5752d156bb8f9c40de23fe39804
Parker D Van Every <pdvanevery@gmail.com>
2023-03-09 19:31:02 -0800

    added function to capitalize first letter of pokemon name and some css

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 function fetchPokemon(){
 
 fetch("https://pokeapi.co/api/v2/pokemon/mudkip")
     .then(res => res.json())
     .then(data => {console.log(data)
         document.querySelector('#name').innerHTML = `
-        <h1>${data.name}</h1> 
-        <img src="${data.sprites.other["official-artwork"].front_default}" alt="${data.name}"/>
+        <h1>${capitalizeFirstLetter(data.name)}</h1> 
+        <img src="${data.sprites.other["official-artwork"].front_default}" alt="${capitalizeFirstLetter(data.name)}"/>
         `
         
     })} 
