916d1c3f847bba48de31d2128c26f95a0c852e6b
Parker D Van Every <pdvanevery@gmail.com>
2023-03-09 19:24:33 -0800

    pokemon sprite displays properly

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ function fetchPokemon(){
 fetch("https://pokeapi.co/api/v2/pokemon/mudkip")
     .then(res => res.json())
     .then(data => {console.log(data)
-        document.querySelector('#name').innerHTML = `<h1>${data.name}</h1>`
+        document.querySelector('#name').innerHTML = `
+        <h1>${data.name}</h1> 
+        <img src="${data.sprites.other["official-artwork"].front_default}" alt="${data.name}"/>
+        `
+        
     })} 
 
 
