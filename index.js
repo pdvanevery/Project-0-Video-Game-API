6fe8c2505b6b50158d4c453552896e90a4f31772
Parker D Van Every <pdvanevery@gmail.com>
2023-03-09 20:11:04 -0800

    added function to make user input become lowercase because that is the only way that the pokmeon api can find mons

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ let btn = document.querySelector('#btn')
 btn.addEventListener('click', fetchPokemon)
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+    return string.charAt(0).toUpperCase() + string.slice(1); //takes letter at index 0, capitalizes it, then adds the rest of the string to it
+}
+
+function makeLowerCase(string) {
+    return string.toLowerCase(); //takes user input and makes it lowercase so pokeapi can find mon
 }
 
 function fetchPokemon(){
 
     const name = document.querySelector('#pokemonName').value
+    const pokeName = makeLowerCase(name)
+    
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
         .then(res => res.json())
         .then(data => {console.log(data)
             document.querySelector('#pokecard').innerHTML = `
