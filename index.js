022e70dbdc1ac323fcc4e690386d8f389009954d
Parker D Van Every <pdvanevery@gmail.com>
2023-03-09 19:45:55 -0800

    removed excess whitespace

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function fetchPokemon(){
 fetch("https://pokeapi.co/api/v2/pokemon/mudkip")
     .then(res => res.json())
     .then(data => {console.log(data)
-        document.querySelector('#name').innerHTML = `
+        document.querySelector('#pokecard').innerHTML = `
         <h1>${capitalizeFirstLetter(data.name)}</h1> 
         <img src="${data.sprites.other["official-artwork"].front_default}" alt="${capitalizeFirstLetter(data.name)}"/>
         `
