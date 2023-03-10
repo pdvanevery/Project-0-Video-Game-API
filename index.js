ed8ab35461d223da20f4a0572240fb05d62eaeb6
Parker D Van Every <pdvanevery@gmail.com>
2023-03-09 19:57:56 -0800

    successfully searches pokemon

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+let btn = document.querySelector('#btn')
 
+btn.addEventListener('click', fetchPokemon)
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -6,18 +8,20 @@ function capitalizeFirstLetter(string) {
 
 function fetchPokemon(){
 
-fetch("https://pokeapi.co/api/v2/pokemon/mudkip")
-    .then(res => res.json())
-    .then(data => {console.log(data)
-        document.querySelector('#pokecard').innerHTML = `
-        <h1>${capitalizeFirstLetter(data.name)}</h1> 
-        <img src="${data.sprites.other["official-artwork"].front_default}" alt="${capitalizeFirstLetter(data.name)}"/>
-        `
+    const name = document.querySelector('#pokemonName').value
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then(res => res.json())
+        .then(data => {console.log(data)
+            document.querySelector('#pokecard').innerHTML = `
+            <h1>${capitalizeFirstLetter(data.name)}</h1> 
+            <img src="${data.sprites.other["official-artwork"].front_default}" alt="${capitalizeFirstLetter(data.name)}"/>
+            `
         
-    })} 
+})} 
 
 
-    fetchPokemon()
+   
     
 
 
